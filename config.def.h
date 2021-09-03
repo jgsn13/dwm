4032e2fba71135e30926db639dd0857296f68745
Joaquim Gregório <joaquim.ibi@gmail.com>
2021-09-03 18:42:23 -0300

    borders = 0 and default gap = 8

--- config.def.h
+++ config.def.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const Gap default_gap        = {.isgap = 1, .realgap = 8, .gappx = 8};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int scalepreview       = 4;        /* Tag preview scaling */
 static const int showbar            = 1;        /* 0 means no bar */
