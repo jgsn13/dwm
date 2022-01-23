3319e92a22cc995867bbc8bce694fe841d5c62cd
Joaquim Gregório <joaquim.ibi@gmail.com>
2022-01-23 08:42:45 -0300

    update: remove colors since it sucks

--- config.def.h
+++ config.def.h
@@ -1,25 +1,25 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 1; /* border pixel of windows */
+static const unsigned int borderpx = 2; /* border pixel of windows */
 static const Gap default_gap = {.isgap = 1, .realgap = 0, .gappx = 0};
 static const unsigned int snap = 32; /* snap pixel */
 static const int scalepreview = 4;   /* Tag preview scaling */
 static const int showbar = 1;        /* 0 means no bar */
-static const int topbar = 0;         /* 0 means bottom bar */
-static const int user_bh = 24; /* 0 means that dwm will calculate bar height, >=
+static const int topbar = 1;         /* 0 means bottom bar */
+static const int user_bh = 22; /* 0 means that dwm will calculate bar height, >=
                                   1 means dwm will user_bh as bar height */
 static const Bool viewontag = True; /* Switch view on tag switch */
 static const char *fonts[] = {
     "JetBrainsMono NF:size=10",
     "Noto Color Emoji:size=10",
 };
-static const char dmenufont[] = "JetBrainsMono NF:size=10";
-static const char col_gray1[] = "#1A1C24";
+static const char dmenufont[] = "JetBrainsMono NF:size=9";
+static const char col_gray1[] = "#0d1012";
 static const char col_gray2[] = "#171716";
 static const char col_gray3[] = "#eeeeee";
-static const char col_gray4[] = "#6bd6a3";
-static const char col_cyan[] = "#2e2835";
+static const char col_gray4[] = "#5491ab";
+static const char col_cyan[] = "#14181c";
 static const char col_bg[] = "#09090c";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
@@ -29,48 +29,25 @@ static const char *colors[][3] = {
 
 /* tagging */
 static const char *tags[] = {"", "", "", "", "", ""};
-// static const char *tags[] = { "", "", "", "", "", "", "", /*"", ""*/
-// };
-// static const char *tags[] = { "web", "dev", "term", "file", "data", "sett",
-// "img" };
-// static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-static const unsigned int ulinepad =
-    0; /* horizontal padding between the underline and tag */
-static const unsigned int ulinestroke =
-    1; /* thickness / height of the underline */
-static const unsigned int ulinevoffset =
-    0; /* how far above the bottom of the bar the line should appear */
-static const int ulineall =
-    0; /* 1 to show underline on all tags, 0 for just the active ones */
+static const unsigned int ulinepad = 0; /* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke = 2; /* thickness / height of the underline */
+static const unsigned int ulinevoffset = 0; /* how far above the bottom of the bar the line should appear */
+static const int ulineall = 0; /* 1 to show underline on all tags, 0 for just the active ones */
 
 static const char *tagsel[][2][2] = {
     /*      norm                          sel       */
     /*  fg          bg              fg          bg  */
-    {{"#f68f00", col_gray1}, {"#f68f00", col_cyan}},
-    {{"#de3c3c", col_gray1}, {"#de3c3c", col_cyan}},
-    {{"#2eff16", col_gray1}, {"#2eff16", col_cyan}},
-    {{"#4285f4", col_gray1}, {"#4285f4", col_cyan}},
-    {{"#fff04f", col_gray1}, {"#fff04f", col_cyan}},
-    {{"#ea3ae1", col_gray1}, {"#ea3ae1", col_cyan}},
-    {{"#00c9c9", col_gray1}, {"#00c9c9", col_cyan}},
-    {{"#8900f2", col_gray1}, {"#8900f2", col_cyan}},
-    {{"#ffffff", col_gray1}, {"#ffffff", col_cyan}},
+    {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
+    {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
+    {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
+    {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
+    {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
+    {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
+    {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
+    {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
+    {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
 };
-
-// static const char *tagsel[][2][2] = {
-//     /*      norm                          sel       */
-//     /*  fg          bg              fg          bg  */
-//     {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
-//     {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
-//     {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
-//     {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
-//     {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
-//     {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
-//     {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
-//     {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
-//     {{col_gray3, col_gray1}, {col_gray4, col_cyan}},
-// };
 
 static const Rule rules[] = {
     /* xprop(1):
@@ -82,12 +59,10 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact = 0.50; /* factor of master area size [0.05..0.95] */
+static const float mfact = 0.52; /* factor of master area size [0.05..0.95] */
 static const int nmaster = 1;    /* number of clients in master area */
-static const int resizehints =
-    0; /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen =
-    0; /* 1 will force focus on the fullscreen window */
+static const int resizehints = 0; /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
@@ -97,7 +72,7 @@ static const Layout layouts[] = {
 };
 
 /* Winicon */
-#define ICONSIZE 18   /* icon size in pixels */
+#define ICONSIZE 16   /* icon size in pixels */
 #define ICONSPACING 4 /* space (pixels) between icon and title */
 
 /* key definitions */
@@ -159,95 +134,97 @@ static const char *setkbmapbr[] = {"setxkbmap", "br", NULL};
 static const char *setkbmapus[] = {"setxkbmap", "us", NULL};
 
 static Key keys[] = {
-    /* modifier                     key        function        argument */
-    {MODKEY, XK_space, spawn, {.v = dmenucmd}},
-    {MODKEY, XK_d, spawn, {.v = roficmd}},
-    {MODKEY, XK_Return, spawn, {.v = stcmd}},
-    {MODKEY | AltMask, XK_Return, spawn, {.v = kittycmd}},
-    {MODKEY, XK_p, spawn, {.v = print_select}},
-    {MODKEY | ControlMask, XK_p, spawn, {.v = print_fullscreen}},
-    {MODKEY | ControlMask | ShiftMask, XK_p, spawn, {.v = print_window}},
-    {MODKEY, XK_l, spawn, {.v = slock}},
-    {MODKEY | AltMask, XK_k, spawn, {.v = volup}},
-    {MODKEY | AltMask, XK_j, spawn, {.v = voldown}},
-    {MODKEY | AltMask, XK_m, spawn, {.v = volmute}},
-    {MODKEY | AltMask, XK_u, spawn, {.v = volunmute}},
-    {MODKEY | AltMask, XK_f, spawn, {.v = rangercmd}},
-    {MODKEY | ControlMask, XK_m, spawn, {.v = bottomcmd}},
-    {MODKEY | AltMask, XK_e, spawn, {.v = emacsclientcmd}},
-    // { MODKEY|ControlMask,           XK_n,      spawn,          {.v = nmtuicmd
-    // } },
-    {MODKEY | ControlMask, XK_s, spawn, {.v = shutdown}},
-    {MODKEY | ControlMask, XK_r, spawn, {.v = reboot}},
-    {MODKEY | ControlMask, XK_g, spawn, {.v = copygittoken}},
-    {MODKEY | ControlMask, XK_e, spawn, {.v = rofimoji}},
-    {MODKEY, XK_e, spawn, {.v = dmenuemoji}},
-    {MODKEY | ShiftMask, XK_c, spawn, {.v = roficalc}},
-    {MODKEY | ControlMask, XK_x, spawn, {.v = xkill}},
-    {MODKEY | ControlMask, XK_b, spawn, {.v = setkbmapbr}},
-    {MODKEY | ControlMask, XK_u, spawn, {.v = setkbmapus}},
-    {MODKEY, XK_b, togglebar, {0}},
-    {MODKEY, XK_j, focusstack, {.i = +1}},
-    {MODKEY, XK_k, focusstack, {.i = -1}},
-    {MODKEY, XK_i, incnmaster, {.i = +1}},
-    {MODKEY, XK_o, incnmaster, {.i = -1}},
-    {MODKEY | ShiftMask, XK_h, setmfact, {.f = -0.05}},
-    {MODKEY | ShiftMask, XK_l, setmfact, {.f = +0.05}},
-    {MODKEY | ShiftMask, XK_Return, zoom, {0}},
-    {MODKEY, XK_Tab, view, {0}},
-    {MODKEY, XK_w, killclient, {0}},
-    {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
-    {MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
-    {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
-    {MODKEY, XK_q, setlayout, {0}},
-    {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
-    {MODKEY | ShiftMask, XK_f, togglefullscr, {0}},
-    {MODKEY, XK_Down, moveresize, {.v = "0x 25y 0w 0h"}},
-    {MODKEY, XK_Up, moveresize, {.v = "0x -25y 0w 0h"}},
-    {MODKEY, XK_Right, moveresize, {.v = "25x 0y 0w 0h"}},
-    {MODKEY, XK_Left, moveresize, {.v = "-25x 0y 0w 0h"}},
-    {MODKEY | ShiftMask, XK_Down, moveresize, {.v = "0x 0y 0w 25h"}},
-    {MODKEY | ShiftMask, XK_Up, moveresize, {.v = "0x 0y 0w -25h"}},
-    {MODKEY | ShiftMask, XK_Right, moveresize, {.v = "0x 0y 25w 0h"}},
-    {MODKEY | ShiftMask, XK_Left, moveresize, {.v = "0x 0y -25w 0h"}},
-    {MODKEY | ControlMask, XK_Up, moveresizeedge, {.v = "t"}},
-    {MODKEY | ControlMask, XK_Down, moveresizeedge, {.v = "b"}},
-    {MODKEY | ControlMask, XK_Left, moveresizeedge, {.v = "l"}},
-    {MODKEY | ControlMask, XK_Right, moveresizeedge, {.v = "r"}},
-    {MODKEY | ControlMask | ShiftMask, XK_Up, moveresizeedge, {.v = "T"}},
-    {MODKEY | ControlMask | ShiftMask, XK_Down, moveresizeedge, {.v = "B"}},
-    {MODKEY | ControlMask | ShiftMask, XK_Left, moveresizeedge, {.v = "L"}},
-    {MODKEY | ControlMask | ShiftMask, XK_Right, moveresizeedge, {.v = "R"}},
-    {MODKEY, XK_0, view, {.ui = ~0}},
-    {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
-    {MODKEY, XK_comma, focusmon, {.i = -1}},
-    {MODKEY, XK_period, focusmon, {.i = +1}},
-    {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
-    {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
-    {MODKEY, XK_n, toggletag, {.ui = 1}},
-    {MODKEY, XK_minus, setgaps, {.i = -2}},
-    {MODKEY, XK_equal, setgaps, {.i = +2}},
-    {MODKEY | ShiftMask, XK_minus, setgaps, {.i = GAP_RESET}},
-    {MODKEY | ShiftMask, XK_equal, setgaps, {.i = GAP_TOGGLE}},
-    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
-        TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 6)
-            TAGKEYS(XK_9, 6){MODKEY | ShiftMask, XK_q, quit, {0}},
+    /* modifier                        key            function           argument */
+    {MODKEY,                           XK_space,      spawn,             {.v = dmenucmd}},
+    {MODKEY,                           XK_d,          spawn,             {.v = roficmd}},
+    {MODKEY,                           XK_Return,     spawn,             {.v = stcmd}},
+    {MODKEY|AltMask,                   XK_Return,     spawn,             {.v = kittycmd}},
+    {MODKEY,                           XK_p,          spawn,             {.v = print_select}},
+    {MODKEY|ControlMask,               XK_p,          spawn,             {.v = print_fullscreen}},
+    {MODKEY|ControlMask|ShiftMask,     XK_p,          spawn,             {.v = print_window}},
+    {MODKEY,                           XK_l,          spawn,             {.v = slock}},
+    {MODKEY|AltMask,                   XK_k,          spawn,             {.v = volup}},
+    {MODKEY|AltMask,                   XK_j,          spawn,             {.v = voldown}},
+    {MODKEY|AltMask,                   XK_m,          spawn,             {.v = volmute}},
+    {MODKEY|AltMask,                   XK_u,          spawn,             {.v = volunmute}},
+    {MODKEY|AltMask,                   XK_f,          spawn,             {.v = rangercmd}},
+    {MODKEY|ControlMask,               XK_m,          spawn,             {.v = bottomcmd}},
+    {MODKEY|AltMask,                   XK_e,          spawn,             {.v = emacsclientcmd}},
+    {MODKEY|ControlMask,               XK_s,          spawn,             {.v = shutdown}},
+    {MODKEY|ControlMask,               XK_r,          spawn,             {.v = reboot}},
+    {MODKEY|ControlMask,               XK_g,          spawn,             {.v = copygittoken}},
+    {MODKEY|ControlMask,               XK_e,          spawn,             {.v = rofimoji}},
+    {MODKEY,                           XK_e,          spawn,             {.v = dmenuemoji}},
+    {MODKEY|ShiftMask,                 XK_c,          spawn,             {.v = roficalc}},
+    {MODKEY|ControlMask,               XK_x,          spawn,             {.v = xkill}},
+    {MODKEY|ControlMask,               XK_b,          spawn,             {.v = setkbmapbr}},
+    {MODKEY|ControlMask,               XK_u,          spawn,             {.v = setkbmapus}},
+    {MODKEY,                           XK_b,          togglebar,         {0}},
+    {MODKEY,                           XK_j,          focusstack,        {.i = +1}},
+    {MODKEY,                           XK_k,          focusstack,        {.i = -1}},
+    {MODKEY,                           XK_i,          incnmaster,        {.i = +1}},
+    {MODKEY,                           XK_o,          incnmaster,        {.i = -1}},
+    {MODKEY|ShiftMask,                 XK_h,          setmfact,          {.f = -0.05}},
+    {MODKEY|ShiftMask,                 XK_l,          setmfact,          {.f = +0.05}},
+    {MODKEY|ShiftMask,                 XK_Return,     zoom,              {0}},
+    {MODKEY,                           XK_Tab,        view,              {0}},
+    {MODKEY,                           XK_w,          killclient,        {0}},
+    {MODKEY,                           XK_t,          setlayout,         {.v = &layouts[0]}},
+    {MODKEY,                           XK_f,          setlayout,         {.v = &layouts[1]}},
+    {MODKEY,                           XK_m,          setlayout,         {.v = &layouts[2]}},
+    {MODKEY,                           XK_q,          setlayout,         {0}},
+    {MODKEY|ShiftMask,                 XK_space,      togglefloating,    {0}},
+    {MODKEY|ShiftMask,                 XK_f,          togglefullscr,     {0}},
+    {MODKEY,                           XK_Down,       moveresize,        {.v = "0x 25y 0w 0h"}},
+    {MODKEY,                           XK_Up,         moveresize,        {.v = "0x -25y 0w 0h"}},
+    {MODKEY,                           XK_Right,      moveresize,        {.v = "25x 0y 0w 0h"}},
+    {MODKEY,                           XK_Left,       moveresize,        {.v = "-25x 0y 0w 0h"}},
+    {MODKEY|ShiftMask,                 XK_Down,       moveresize,        {.v = "0x 0y 0w 25h"}},
+    {MODKEY|ShiftMask,                 XK_Up,         moveresize,        {.v = "0x 0y 0w -25h"}},
+    {MODKEY|ShiftMask,                 XK_Right,      moveresize,        {.v = "0x 0y 25w 0h"}},
+    {MODKEY|ShiftMask,                 XK_Left,       moveresize,        {.v = "0x 0y -25w 0h"}},
+    {MODKEY|ControlMask,               XK_Up,         moveresizeedge,    {.v = "t"}},
+    {MODKEY|ControlMask,               XK_Down,       moveresizeedge,    {.v = "b"}},
+    {MODKEY|ControlMask,               XK_Left,       moveresizeedge,    {.v = "l"}},
+    {MODKEY|ControlMask,               XK_Right,      moveresizeedge,    {.v = "r"}},
+    {MODKEY|ControlMask|ShiftMask,     XK_Up,         moveresizeedge,    {.v = "T"}},
+    {MODKEY|ControlMask|ShiftMask,     XK_Down,       moveresizeedge,    {.v = "B"}},
+    {MODKEY|ControlMask|ShiftMask,     XK_Left,       moveresizeedge,    {.v = "L"}},
+    {MODKEY|ControlMask|ShiftMask,     XK_Right,      moveresizeedge,    {.v = "R"}},
+    {MODKEY,                           XK_0,          view,              {.ui = ~0}},
+    {MODKEY|ShiftMask,                 XK_0,          tag,               {.ui = ~0}},
+    {MODKEY,                           XK_comma,      focusmon,          {.i = -1}},
+    {MODKEY,                           XK_period,     focusmon,          {.i = +1}},
+    {MODKEY|ShiftMask,                 XK_comma,      tagmon,            {.i = -1}},
+    {MODKEY|ShiftMask,                 XK_period,     tagmon,            {.i = +1}},
+    {MODKEY,                           XK_n,          toggletag,         {.ui = 1}},
+    {MODKEY,                           XK_minus,      setgaps,           {.i = -2}},
+    {MODKEY,                           XK_equal,      setgaps,           {.i = +2}},
+    {MODKEY|ShiftMask,                 XK_minus,      setgaps,           {.i = GAP_RESET}},
+    {MODKEY|ShiftMask,                 XK_equal,      setgaps,           {.i = GAP_TOGGLE}},
+    TAGKEYS(XK_1, 0)
+    TAGKEYS(XK_2, 1)
+    TAGKEYS(XK_3, 2)
+    TAGKEYS(XK_4, 3)
+    TAGKEYS(XK_5, 4)
+    TAGKEYS(XK_6, 5)
+    {MODKEY | ShiftMask, XK_q, quit, {0}},
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
  * ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-    /* click                event mask      button          function argument
+    /* click                event mask      button          function            argument
      */
-    {ClkLtSymbol, 0, Button1, setlayout, {0}},
-    {ClkLtSymbol, 0, Button3, setlayout, {.v = &layouts[2]}},
-    {ClkWinTitle, 0, Button2, zoom, {0}},
-    {ClkClientWin, MODKEY, Button1, movemouse, {0}},
-    {ClkClientWin, MODKEY, Button2, togglefloating, {0}},
-    {ClkClientWin, MODKEY, Button3, resizemouse, {0}},
-    {ClkTagBar, 0, Button1, view, {0}},
-    {ClkTagBar, 0, Button3, toggleview, {0}},
-    {ClkTagBar, MODKEY, Button1, tag, {0}},
-    {ClkTagBar, MODKEY, Button3, toggletag, {0}},
+    {ClkLtSymbol,           0,              Button1,        setlayout,          {0}},
+    {ClkLtSymbol,           0,              Button3,        setlayout,          {.v = &layouts[2]}},
+    {ClkWinTitle,           0,              Button2,        zoom,               {0}},
+    {ClkClientWin,          MODKEY,         Button1,        movemouse,          {0}},
+    {ClkClientWin,          MODKEY,         Button2,        togglefloating,     {0}},
+    {ClkClientWin,          MODKEY,         Button3,        resizemouse,        {0}},
+    {ClkTagBar,             0,              Button1,        view,               {0}},
+    {ClkTagBar,             0,              Button3,        toggleview,         {0}},
+    {ClkTagBar,             MODKEY,         Button1,        tag,                {0}},
+    {ClkTagBar,             MODKEY,         Button3,        toggletag,          {0}},
 };
