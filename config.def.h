53e2e7cfb42141d786dc450df25be0c28b57797b
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-10-01 20:16:37 -0300

    update: set default terminal to kitty

--- config.def.h
+++ config.def.h
@@ -87,7 +87,7 @@ static const Layout layouts[] = {
 static const char *dmenucmd[] = {
     "dmenu_run", "-fn",    dmenufont, "-nb",     col_gray1, "-nf", col_gray3,
     "-sb",       col_cyan, "-sf",     col_gray4, "-c",      NULL};
-static const char *termcmd[] = {"st", NULL};
+static const char *termcmd[] = {"kitty", NULL};
 
 // look at the scripts folder
 static const char *dmenuemoji[] = {"dmenuunicode", NULL};
